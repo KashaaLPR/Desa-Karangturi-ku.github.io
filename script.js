228d02f5b54bdb36e6e257190afae7bbde7fde71
KashaaLPR <rerampedan16@gmail.com>
2026-06-02 09:50:45 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const welcome = document.getElementById("welcome");
+
+    if (welcome) {
+        welcome.innerHTML =
+            "Selamat Datang di Website Desa Karangturi";
+    }
+
+});
