e16013d6a07a8b2ed69deab8badba1eb2ee4cb05
KashaaLPR <rerampedan16@gmail.com>
2026-06-04 07:18:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,15 @@ cards.forEach(card => {
     observer.observe(card);
 
 });
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+
+    if(link.href === window.location.href){
+
+        link.classList.add("active");
+
+    }
+
+});
