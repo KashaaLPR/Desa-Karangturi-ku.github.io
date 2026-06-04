68c9212655ce2049c5c85915b413c0d49526fdd6
KashaaLPR <rerampedan16@gmail.com>
2026-06-04 07:11:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,25 @@ if (salam) {
     salam.textContent = pesan;
 
 }
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+cards.forEach(card => {
+
+    observer.observe(card);
+
+});
