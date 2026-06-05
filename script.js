78cbc256cb1c0abf60b9555721b6460f5a583545
KashaaLPR <rerampedan16@gmail.com>
2026-06-05 08:35:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,18 @@ cards.forEach(card => {
 
 });
 
+closePopup.addEventListener("click", () => {
+
+    popup.style.display = "none";
+
+});
+
+popup.addEventListener("click", (e) => {
+
+    if(e.target === popup){
+
+        popup.style.display = "none";
+
+    }
+
+});
